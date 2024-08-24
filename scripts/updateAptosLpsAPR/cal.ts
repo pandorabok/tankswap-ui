825ab9944edef156f8bbd355d3640b3c73d83236
pandorabok <borkovicbiljana9@gmail.com>
2024-08-24 11:58:00 -0700

    Contract development update - added 4 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -539,3 +539,7 @@ fetchAndUpdateAptosLPsAPR()
 
 import fs from 'fs'
 import os from 'os'
+import fetch from 'node-fetch'
+import BigNumber from 'bignumber.js'
+import chunk from 'lodash/chunk'
+import { ChainId, Pair } from '@tankswap/aptos-swap-sdk'
