8f1572651d2b80178bacf1ca6b9d20a2d16cd614
pandorabok <borkovicbiljana9@gmail.com>
2024-09-02 15:02:00 -0700

    Contract development update - added 4 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -529,3 +529,7 @@ const fetchAndUpdateAptosLPsAPR = async () => {
     allAprs = { ...allAprs, ...aprs }
   }
 
+  fs.writeFile(`apps/aptos/config/constants/lpAprs/1.json`, JSON.stringify(allAprs, null, 2) + os.EOL, (err) => {
+    if (err) throw err
+    console.info(` ✅ - lpAprs.json has been updated!`)
+  })
