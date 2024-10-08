ae69a8d74c967cc85df0e030abfd184d1f88a900
pandorabok <borkovicbiljana9@gmail.com>
2024-10-08 15:50:00 -0700

    Contract development update - added 2 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -486,3 +486,5 @@ const fetchFarmsOneWeekAgo = async (farmsAtLatestBlock: SingleFarmResponse[]) =>
 }
 
 const getAprsForFarmGroup = async (addresses: string[]): Promise<any> => {
+  try {
+    const farmsAtLatestBlock = await fetchFarmLpsInfo(addresses)
