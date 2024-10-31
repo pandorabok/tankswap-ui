ad22bbd29a52b1e158416956ca3c3811f9623b58
pandorabok <borkovicbiljana9@gmail.com>
2024-10-30 18:50:00 -0700

    Contract development update - added 3 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -437,3 +437,6 @@ const fetchFarmsOneWeekAgo = async (farmsAtLatestBlock: SingleFarmResponse[]) =>
   farmsAtLatestBlock.forEach((farm) => {
     if (response[farm.id]) {
       if (response[farm.id].updateDate !== currentDate) {
+        const isMoreThanAWeek = response[farm.id].usdList.length >= 7
+        const usdList = [...(response[farm.id]?.usdList || [])]
+
