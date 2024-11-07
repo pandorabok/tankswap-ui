ca9af12efd0938123039d69c71a21a4116b095f9
pandorabok <borkovicbiljana9@gmail.com>
2024-11-07 09:19:00 -0800

    Contract development update - added 5 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -421,3 +421,8 @@ const fetchFarmLpsInfo = async (addresses: string[]): Promise<SingleFarmResponse
       }
     })
 
+    return {
+      id: address,
+      volumeUSD: farmPriceInfo?.volumeUsd24h || '0',
+      reserveUSD: farmPriceInfo?.liquidity || '0',
+    }
