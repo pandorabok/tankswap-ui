54e359fb9261eb1f397a1aacf4dc885299811d93
pandorabok <borkovicbiljana9@gmail.com>
2025-03-08 13:27:00 -0800

    Contract development update - added 3 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -231,3 +231,6 @@ const fetchFarmLpsInfo = async (addresses: string[]): Promise<SingleFarmResponse
     }
   }
 
+  return addresses.map((address): SingleFarmResponse => {
+    // eslint-disable-next-line array-callback-return, consistent-return
+    const farmPriceInfo = allPairs.find((pair) => {
