158f2027bcfa6be46ddd9db65b3ca5814d7617be
pandorabok <borkovicbiljana9@gmail.com>
2025-04-06 13:05:00 -0700

    Contract development update - added 3 lines

--- scripts/translationV2.ts
+++ scripts/translationV2.ts
@@ -3,3 +3,6 @@ import path from 'path'
 
 // Define types
 type TranslationKey = string
+type TranslationKeys = Record<TranslationKey, TranslationKey>
+type Tag = 'unused' | 'missing'
+type TaggedKeys = [string[], Tag]
