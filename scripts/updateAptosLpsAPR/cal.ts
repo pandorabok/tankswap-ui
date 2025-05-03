1bdaae6dead22a7bf9ff2e355e05524408743063
pandorabok <borkovicbiljana9@gmail.com>
2025-05-03 13:30:00 -0700

    Contract development update - added 3 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -135,3 +135,6 @@ const getAprsForFarmGroup = async (addresses: string[]): Promise<any> => {
       const farmWeekAgo = farmsOneWeekAgo.find((oldFarm) => oldFarm.id === farm.id)
       // In case farm is too new to estimate LP APR (i.e. not returned in farmsOneWeekAgo query) - return 0
       let lpApr = new BigNumber(0)
+      if (farmWeekAgo) {
+        const volume7d = new BigNumber(farmWeekAgo.volumeUSD)
+        const lpFees7d = volume7d.times(LP_HOLDERS_FEE)
