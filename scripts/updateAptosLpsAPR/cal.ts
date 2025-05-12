d2c3409a232ff86929ba635def97c27fc231b62d
pandorabok <borkovicbiljana9@gmail.com>
2025-05-12 09:43:00 -0700

    Contract development update - added 4 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -119,3 +119,7 @@ const fetchFarmsOneWeekAgo = async (farmsAtLatestBlock: SingleFarmResponse[]) =>
     const { usdList } = responseData[address]
 
     if (usdList.length > 0) {
+      volumeUSD = usdList.reduce((sum, single) => new BigNumber(sum).plus(single.volumeUSD).toNumber(), 0).toString()
+      reserveUSD = usdList.reduce((sum, single) => new BigNumber(sum).plus(single.reserveUSD).toNumber(), 0).toString()
+    }
+    return { id: address, volumeUSD, reserveUSD }
