855baeed7b7a5ff464b99108d06877e2ea8c025d
pandorabok <borkovicbiljana9@gmail.com>
2025-07-19 16:29:00 -0700

    Contract development update - added 1 lines

--- scripts/updateAptosLpsAPR/cal.ts
+++ scripts/updateAptosLpsAPR/cal.ts
@@ -37,3 +37,4 @@ const fetchFarmLpsInfo = async (addresses: string[]): Promise<SingleFarmResponse
   const allPairs: any = []
   const maxLoop = 20 // 50 * 20 = max get 1000 pair
   for (let i = 0; i < maxLoop; i++) {
+    const offset = i === 0 ? 1 : 50 * i + 1
